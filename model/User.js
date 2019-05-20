7a66fc381aff55bffc0b1c7d3fa4141e804e70b3
Luis <lherreral@gmail.com>
2019-05-19 22:15:00 -0400

    Add end points:
    - /users/me: GET Method
    - /users/me/photo: PUT Method
    - /users/me/update: PUT Method
    - /users/me/change-password: PUT Method
    
    Move locations jsons from model to seeds folder

--- model/User.js
+++ model/User.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userScheme = Schema({
-    fullname: { type: String },
+    fullname: { type: String, default: '' },
     username: { type: String, unique: true, sparse: true },
     email: { type: String, unique: true, required: true },
     password: { type: String },
     token: { type: String },
     creation_date: { type: Date, default: Date.now },
     following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    photo: { type: String },
 
     googleId: { type: String },
     updated_at: { type: Date, default: Date.now },
