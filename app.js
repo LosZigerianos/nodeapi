a80a52161f6115e0d9dd3330076ef5dae066e9ad
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2019-06-19 11:20:40 -0400

    Add cors configuration

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const passport = require('passport');
-var i18n = require("./lib/i18n");
+const i18n = require("./lib/i18n");
 const api = require('./webservice/api');
+const cors = require('cors');
 
-var app = express();
+const app = express();
+
+// Access-Control-Allow-Origin: *
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
